52a94c5484d46015a738b0758ee03793a4fc0c9f
Gustavo Diniz da Corte <gustavodacorte@gmail.com>
2026-07-03 16:56:23 +0900

    preparing ot integrate big guns
    TODO: DYnamoTesting

--- src/gdcutils/db/postgres/__init__.py
+++ src/gdcutils/db/postgres/__init__.py
@@ -1,9 +1,11 @@
 """PostgreSQL Handlers."""
 
+from .config import PgConnectionParams
 from .exceptions import PgBaseError, PgConstraintViolation
 from .service import pg_conn, pg_cursor, pg_fastapi_lifespan, pg_pool_lifespan
 
 __all__ = [
+    "PgConnectionParams",
     "PgBaseError",
     "PgConstraintViolation",
     "pg_conn",
